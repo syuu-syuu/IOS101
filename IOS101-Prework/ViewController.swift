34c525097280291158666400581be6e362565eb6
Lizzy Zhou <zhongle@unc.edu>
2024-02-04 17:46:11 -0500

    prework

--- IOS101-Prework/ViewController.swift
+++ IOS101-Prework/ViewController.swift
@@ -13,7 +13,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func changeColor() -> UIColor{
 
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
 
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
 }
 
